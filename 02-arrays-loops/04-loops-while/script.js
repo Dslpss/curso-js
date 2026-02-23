eadf022b93c6c495c7370741eb99791d96c0c826
Dslpss <dennisemannuel93@gmail.com>
2026-02-23 10:33:48 -0300

    loop - while

---
+++ 02-arrays-loops/04-loops-while/script.js
@@ -0,0 +1,33 @@
+
+//!Loop while
+console.log(`\nNúmeros pares:`);
+let i = 2;
+while (i < 10) {
+  console.log(i);
+  i += 2;
+}
+
+let postagens = [
+  "imagem1",
+  "imagem2",
+  "imagem3",
+  "imagem4",
+]
+let p = 0
+
+while (p < postagens.length) {
+  console.log(`${postagens}`);
+  postagens++
+}
+
+// console.log(`Contagem de 1 a 10:`);
+// for (j = 1; j <= 10; j++) {
+//   console.log(j);
+
+// }
+
+// let numero = 10
+// do {
+//   console.log("Executando uma vez.");
+//   numero++
+// }while(numero < 5)
